956ffb4a4c26682e60053b79943934602838c629
Andrei Lungu <andreilungu777@yahoo.ro>
2026-05-24 20:43:00 +0300

    Replaced ts-node with tsx

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import corsOptions from "./config/corsOptions";
-
 import registerRouter from "./routes/api/register";
 import loginRouter from "./routes/api/login";
 import refreshRouter from "./routes/api/refresh";
